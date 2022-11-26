2dbc6d126e5cb99e49a528088d89cc92e6b22d29
LuisOrescoEOTW <lhorescovich@gmail.com>
2022-11-26 10:34:52 -0300

    nuevos cambios

---
+++ src/app/provincias-select/provincias-select.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-provincias-select',
+  templateUrl: './provincias-select.component.html',
+  styleUrls: ['./provincias-select.component.css']
+})
+export class ProvinciasSelectComponent {
+
+}
